1fb2156aa5d476dc6a37ae7b0c34f3e11c0a0e6a
takoleo <mylovelyphoto93@gmail.com>
2024-10-02 14:49:04 +0200

    Update operation.js

--- operation.js
+++ operation.js
@@ -1,33 +1,68 @@
 
-//function operation
-
-function calculator(event){
-    event.preventDefault();
-    console.log(event)
-
-    const ValueOfFirstNumber=document.getElementById("firstNumber").value;
-    console.log(ValueOfFirstNumber, typeof ValueOfFirstNumber);
-
-    const ValueOfSecondNumber=document.getElementById("secondNumber").value;
-    console.log(ValueOfSecondNumber,typeof ValueOfSecondNumber)
-
-    const operation=document.getElementById("operation").value;
-    console.log(operation,typeof operation);
-
-    let result=0;
-    if(operation === "1"){
-        result =(1*ValueOfFirstNumber) + (1*ValueOfSecondNumber);
-    }else if (operation==="2"){
-        result=parseInt(ValueOfFirstNumber)* parseInt(ValueOfSecondNumber);
-    }else if (operation==="3"){
-        result=parseInt(ValueOfFirstNumber)-parseInt(ValueOfSecondNumber);
-    }else if(operation==="4"){
-        result=parseInt(ValueOfFirstNumber)/ parseInt(ValueOfSecondNumber);
-    }else if(operation==="5"){
-        result=parseInt(ValueOfFirstNumber)%parseInt(ValueOfSecondNumber);
-    }
-    document.getElementById("result").innerHTML= `${result}`
-    console.log(result,typeof result);
+ function calculator(event){
+ event.preventDefault();
 
 
-}
+  console.log("make calculation");
+  const firstNumber= document.getElementById("firstNumber");
+  const ValueOfFirstNumber = firstNumber.value;
+  console.log(ValueOfFirstNumber);
+
+  const secondNumber= document.getElementById("secondNumber");
+  const ValueOfSecondNumber = secondNumber.value;
+  console.log(ValueOfSecondNumber);
+
+  const operation = document.getElementById("operation");
+  const selectedOperation = operation.value;
+  console.log(selectedOperation)
+  console.log(selectedOperation)
+  let result =0;
+  if(Number(selectedOperation) === 1){
+      result = parseInt(ValueOfFirstNumber) + parseInt(ValueOfSecondNumber);
+
+  }else if(Number(selectedOperation) === 2){
+   result =  Number(ValueOfFirstNumber) * Number(ValueOfSecondNumber);
+
+  }else if (Number(selectedOperation) ===3){
+   result =  Number(ValueOfFirstNumber) - Number(ValueOfSecondNumber);
+
+  }else if (Number(selectedOperation) ===4){
+   result =  Number(ValueOfFirstNumber) / Number(ValueOfSecondNumber);
+
+  }else if (Number(selectedOperation) ===5){
+   result= parseInt(ValueOfFirstNumber) % parseInt(ValueOfSecondNumber);
+  }
+
+  const Name= document.getElementById("name");
+  const ValueOfName = Name.value;
+  console.log( ValueOfName);
+
+  //Initiales
+  const nameAndSurname =ValueOfName.split(" ");
+  const initials = `${nameAndSurname[0][0]} ${nameAndSurname[1][0]}`;
+
+  // nameAndSurname[0][0];
+  // nameAndSurname[1][0];
+
+  const op= document.getElementById("operation");
+  // const value = op.options[op.selectedIndex].value
+  const text =op.options[op.selectedIndex].text;
+  console.log(text);
+
+  console.log("================================")
+  console.log(nameAndSurname);
+  console.log("================================")
+
+  //concatenation
+  let nameAndResult = ValueOfName+" " + " "+result;
+  nameAndResult = ` ${ValueOfName} ` + `${text}`+ " la  de 6 et de 5 est " + `${result}`;
+  console.log("result " + result, typeof result);
+
+  // nameAndResult = `${nameAndResult} ${result}`
+  document.getElementById("result").innerHTML = nameAndResult;
+
+  // length of a character chain
+    const length = nameAndResult.length;
+  console.log("length  " + length);
+
+ }
